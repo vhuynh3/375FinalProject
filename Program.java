c8aedc96028986da3616587cdb03aead8639b035
vhuynh3 <vhuynh3@cs.binghamton.edu>
2022-11-30 10:45:00 -0500

    added command line arguments to run program

--- Program.java
+++ Program.java
@@ -15,16 +15,51 @@ public class Program {
     public static final int ALPHABETA = 2;
     public static final int ITER = 3;
 
+    public static final String usage = "usage: java Program <RANDOM/MINIMAX/ALPHABETA/ITER> <RANDOM/MINIMAX/ALPHABETA/ITER> <# of Games> <output file>";
+
     public static void main(String[] args) {
+        //java Program RANDOM RANDOM 100 output.txt
+
+        if (args.length < 2) {
+            System.out.println(usage);
+            System.exit(0);
+        }
+        int p1 = RANDOM;
+        int p2 = RANDOM;
+
+        if (args[0].equals("RANDOM")) {p1 = RANDOM;}
+        else if (args[0].equals("MINIMAX")) {p1 = MINIMAX;}
+        else if (args[0].equals("ALPHABETA")) {p1 = ALPHABETA;}
+        else if (args[0].equals("ITER")) {p1 = ITER;}
+        else {
+            System.out.println("Player 1 type invalid. Using RANDOM as default");
+        }
+
+        if (args[1].equals("RANDOM")) {p2 = RANDOM;}
+        else if (args[1].equals("MINIMAX")) {p2 = MINIMAX;}
+        else if (args[1].equals("ALPHABETA")) {p2 = ALPHABETA;}
+        else if (args[1].equals("ITER")) {p2 = ITER;}
+        else {
+            System.out.println("Player 2 type invalid. Using RANDOM as default");
+        }
+
+        int numGames = 100; //default 100
+        try {
+            numGames = Integer.parseInt(args[2]);
+        } catch(NumberFormatException e) {
+            System.out.println("Invalid number of games. Using 100 as default.");
+        }
+
+
         Game game;
         int result;
 
-        int p1wins = 0; //min
-        int p2wins = 0; //max
+        int p1wins = 0; //min player
+        int p2wins = 0; //max player
         int tie = 0;
 
-        for (int i = 0; i < 100; i++) {
-            game = new Game(MINIMAX, RANDOM);
+        for (int i = 0; i < numGames; i++) {
+            game = new Game(p1, p2);
             result = game.playGame();
             if (result > 0) {
                 p2wins ++;
@@ -35,9 +70,25 @@ public class Program {
             }
         }
 
-        System.out.println("player 1 wins: " + p1wins);
-        System.out.println("player 2 wins: " + p2wins);
-        System.out.println("ties: "+ tie);
+        float p1Rate = p1wins * 100/numGames ;
+        float p2Rate = p2wins* 100/numGames;
+        float tieRate = tie* 100/numGames;
+
+        File f;
+        FileWriter w;
+        try {
+            f = new File(args[3]);
+            w = new FileWriter(f);
+            
+            w.write("player 1 wins: " + p1Rate + "%\n");
+            w.write("player 2 wins: " + p2Rate + "%\n");
+            w.write("ties: " + tieRate + "%");
+
+            w.close();
+        } catch (IOException e) {
+            System.out.println("Could not open " + args[3] + ". Try again.");
+            System.exit(0);
+        }
 
     }
 }
